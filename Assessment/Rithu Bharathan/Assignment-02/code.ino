88b5c2e778181a8aaa71e16dc41825e4ed00a2e0
Rithubharathan <113875565+Rithubharathan@users.noreply.github.com>
2022-10-19 09:50:36 +0530

    Add files via upload

---
+++ Assessment/Rithu Bharathan/Assignment-02/code.ino
@@ -0,0 +1,24 @@
+from random import randint
+from time import sleep
+
+iterator = 0
+
+while(True):
+    tmp = randint(0,75)
+    print("Current Temperature : ",tmp,sep="\t")
+    humdty = randint(0,100)
+    print("Current Humidity : ",humdty,sep="\t",end="\n")
+    
+    if(not(10<tmp<42)):
+        print("Unbearable Temperature")
+
+    if(not(25<humdty<75)):
+        print("Unbearable Humidity")
+
+    print("\n\n")
+
+    sleep(1)
+    iterator+=1
+
+    if(iterator>100):
+        break
